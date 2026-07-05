3d9a3ea028d9da9ecddfcc866ad17438c7098538
Hamza Shinan <hamzashinan002@gmail.com>
2026-07-05 21:38:37 +0530

    Update deployment configuration

--- task-manager/backend/config/settings.py
+++ task-manager/backend/config/settings.py
@@ -153,7 +153,9 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://task-manager-zeta-azure.vercel.app",
+]
 
 from datetime import timedelta
 
